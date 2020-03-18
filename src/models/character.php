45e82343e230d7598aad89ddd90d426bcbeee91e
NunzioArdi <nunzio.conte2@etu.univ-lorraine.fr>
2020-03-18 10:29:11 +0100

    fonction join

--- src/models/character.php
+++ src/models/character.php
@@ -7,5 +7,25 @@ class character extends \Illuminate\Database\Eloquent\Model{
 
     protected $table='character';
     protected $primaryKey='id';
+    protected $fillable = [ 'name', 'real_name', 'last_name','alias',
+        'deck', 'description',
+        'birthday', 'gender', 'first_appeared_in_game_id'
+    ];
 
+    public function first_appeared_in_game() {
+
+        return $this->belongsTo('\games\model\Game', 'first_appeared_in_game_id');
+    }
+
+    public function games() {
+        return $this->belongsToMany('\games\model\Game', 'game2character', 'character_id', 'game_id');
+    }
+
+    public function friends() {
+        return $this->belongsToMany('\games\model\Character', 'friends', 'char1_id', 'char2_id');
+    }
+
+    public function enemies() {
+        return $this->belongsToMany('\games\model\Character', 'enemies', 'char1_id', 'char2_id');
+    }
 }
