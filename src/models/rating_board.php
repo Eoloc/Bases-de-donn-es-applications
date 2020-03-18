c2d56dc31a39b7b3d1d463442b804369b032b950
root <root@LAPTOP-J5GR8B72.localdomain>
2020-03-18 16:57:23 +0100

    ajout de la classe rating_board sans methode Models

--- src/models/rating_board.php
+++ src/models/rating_board.php
@@ -4,6 +4,9 @@ namespace bdd\models;
 use Illuminate\Database\Eloquent\Model;
 
 class rating_board extends Model{
+    protected $table='rating_board';
+    protected $primaryKey='id';
 
+    
 
 }
