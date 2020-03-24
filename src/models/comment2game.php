f8e8d7ea05b529d049693cb54fc2791689a24703
Fabien <fabien.drommer@gmail.com>
2020-03-24 15:31:32 +0100

    ajout comment2game/user

---
+++ src/models/comment2game.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace bdd\models;
+use Illuminate\Database\Eloquent\Model;
+
+class comment2game extends Model{
+    protected $table='comment2game';
+    protected $primaryKey='id';
+
+    
+
+}
---
+++ src/models/comment2user.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace bdd\models;
+use Illuminate\Database\Eloquent\Model;
+
+class comment2user extends Model{
+    protected $table='comment2user';
+    protected $primaryKey='id';
+
+    
+
+}
