397d37bcd1b6676e623cfcf44effb9b6b79e0224
Fabien <fabien.drommer@gmail.com>
2020-03-24 14:51:07 +0100

    modeles user/comment

---
+++ src/models/comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace bdd\models;
+use Illuminate\Database\Eloquent\Model;
+
+
+class comment extends Model{
+
+    protected $table='comment';
+    protected $primaryKey='id';
+    protected $times
+
+}
---
+++ src/models/user.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace bdd\models;
+use Illuminate\Database\Eloquent\Model;
+
+
+class user extends Model{
+
+    protected $table='user';
+    protected $primaryKey='email';
+
+}
