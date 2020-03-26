44384c69aa165bf01e97bbd3c03da2a43298de25
gatienddr <didry12u@LAPTOP-J5GR8B72.localdomain>
2020-03-26 11:45:24 +0100

    avancement dans la partie 8

--- partie8.php
+++ partie8.php
@@ -12,9 +12,57 @@ $db->addConnection(parse_ini_file('src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 ini_set('memory_limit', '-1');
+
+if(isset($_POST["email"]) and isset($_POST["titre"]) and isset($_POST["contenu"])){
+echo "convertion au format JSON...";
+//$arr= array(
+}
+else{/**
 echo '<form action="action.php" method="post">
- <p>Email utilisateur : <input type="text" name="nom_user" /></p>
- <p>Nom du jeu : <input type="text" name="nom_jeu" /></p>
+ <p>Email utilisateur : <input type="text" name="email_user" /></p>
+ <p>Titre commentaire <input type="text" name="titre_commentaire" /></p>
  <p>Votre commentaire: <input type="text" size="70" name="commentaire" /></p>
- <p><input type="submit" value="OK"></p>
+ <p><input type="submit" formaction="./partie8.php" value="OK"></p>
 </form>';
+*/
+echo '<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Ajouter un commentaire</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="email">Email</label>  
+  <div class="col-md-4">
+  <input name="email" class="form-control input-md" id="email" required="" type="text" placeholder="adresse mail">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="titre">Titre</label>  
+  <div class="col-md-4">
+  <input name="titre" class="form-control input-md" id="titre" required="" type="text" placeholder="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="contenu">Contenu</label>  
+  <div class="col-md-4">
+  <input name="contenu" class="form-control input-md" id="contenu" required="" type="text" placeholder="message">
+    
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="valider"></label>
+  <div class="col-md-4">
+    <button name="valider" class="btn btn-primary" id="valider">Valider</button>
+  </div>
+</div>
+</fieldset>
+</form>';
+}
